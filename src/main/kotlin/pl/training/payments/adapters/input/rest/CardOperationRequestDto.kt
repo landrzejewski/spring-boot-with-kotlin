e14af19b4043d906602b992f2095fa9f13b2101e
Admin <landrzejewski.poczta@gmail.com>
2025-06-25 14:45:03 +0200

    spring data

--- src/main/kotlin/pl/training/payments/adapters/input/rest/CardOperationRequestDto.kt
+++ src/main/kotlin/pl/training/payments/adapters/input/rest/CardOperationRequestDto.kt
@@ -1,5 +1,9 @@
 package pl.training.payments.adapters.input.rest
 
+import java.math.BigDecimal
+
 class CardOperationRequestDto(
+    val amount: BigDecimal,
+    val currencyCode: String,
     val operationType: OperationTypeDto
 )
