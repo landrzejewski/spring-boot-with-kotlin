5bbf61dabcf36faf9e691ff2cf25181d03b7f6e1
Admin <landrzejewski.poczta@gmail.com>
2025-06-25 14:14:14 +0200

    spring data

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,10 @@ repositories {
 val jacksonVersion = "2.17.2"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
