4fdfb657a6cb3e4b28c0dcac9bec9c22e16c421e
Admin <landrzejewski.poczta@gmail.com>
2025-06-25 12:56:07 +0200

    spring data

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
     implementation("org.mapstruct:mapstruct:1.6.2")
     kapt("org.mapstruct:mapstruct-processor:1.6.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
